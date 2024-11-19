84e7c0db7968599e680276c66eba380a406f9a4e
nijahn <strentzn@gmail.com>
2024-11-19 23:14:20 +0100

    modification app.py

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from math import radians, sin, cos, sqrt, atan2
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-GRAPHOPPER_API_KEY = '715a3b7b-0aa4-4829-9429-6ba3038dd58f'
-GRAPHOPPER_URL = "https://graphhopper.com/api/1/route"
-CHARGETRIP_CLIENT_KEY = '673a3742aad68b4b27a41e79'
-CHARGETRIP_APP_KEY = '673a3742aad68b4b27a41e7b'
-CHARGETRIP_URL = 'https://api.chargetrip.io/graphql'
+API_KEY_ROUTE = '715a3b7b-0aa4-4829-9429-6ba3038dd58f'
+ROUTING_SERVICE_URL = "https://graphhopper.com/api/1/route"
+EV_CHARGING_CLIENT_ID = '673d0ac8bd45c1778cd88d28'
+EV_CHARGING_APP_ID = '673d0ac8bd45c1778cd88d2a'
+EV_CHARGING_ENDPOINT = 'https://api.chargetrip.io/graphql'
 
-# Variable globale pour stocker les données des véhicules
-vehicles_data = []
+vehicles_info = []
 
-graphql_query_vehicles = """
-query vehicleListAll {
+graphql_query_cars = """
+query fetchAllVehicles {
   vehicleList {
     id
     naming {
@@ -48,140 +47,116 @@ query vehicleListAll {
 }
 """
 
-def fetch_vehicles_data():
-    global vehicles_data
+def retrieve_vehicle_data():
+    global vehicles_info
     headers = {
-        'x-client-id': CHARGETRIP_CLIENT_KEY,
-        'x-app-id': CHARGETRIP_APP_KEY,
-        'Authorization': f'Bearer {CHARGETRIP_CLIENT_KEY}',
+        'x-client-id': EV_CHARGING_CLIENT_ID,
+        'x-app-id': EV_CHARGING_APP_ID,
+        'Authorization': f'Bearer {EV_CHARGING_CLIENT_ID}',
         'Content-Type': 'application/json'
     }
-    try:
-        response = requests.post(CHARGETRIP_URL, json={'query': graphql_query_vehicles}, headers=headers)
-        response.raise_for_status()
-        vehicles_data = response.json().get('data', {}).get('vehicleList', [])
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching vehicles data: {e}")
-        vehicles_data = []
+    response = requests.post(EV_CHARGING_ENDPOINT, json={'query': graphql_query_cars}, headers=headers)
+    response.raise_for_status()
+    vehicles_info = response.json().get('data', {}).get('vehicleList', [])
 
-@app.route('/')
-def index():
-    try:
-        fetch_vehicles_data()
-        return render_template('index.html', vehicles=vehicles_data)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+@application.route('/')
+def home():
+    retrieve_vehicle_data()
+    return render_template('home.html', vehicles=vehicles_info)
 
-@app.route('/route_data', methods=['POST'])
-def route_data():
-    ville_depart = request.form['start_city']
-    ville_arrivee = request.form['end_city']
-    vehicule_id = request.form['vehicle_id']
+@application.route('/calculate_route', methods=['POST'])
+def calculate_route():
+    origin_city = request.form['origin_city']
+    destination_city = request.form['destination_city']
+    car_id = request.form['car_id']
 
-    try:
-        # Récupérer les informations du véhicule
-        vehicle_data = next((v for v in vehicles_data if v['id'] == vehicule_id), None)
-        
-        if not vehicle_data:
-            return jsonify({'error': 'Vehicle not found'}), 404
+    selected_vehicle = next((car for car in vehicles_info if car['id'] == car_id), None)
+    if not selected_vehicle:
+        return jsonify({'error': 'Vehicle not found'}), 404
 
-        # Détails du véhicule
-        vehicle_details = {
-            'make': vehicle_data['naming']['make'],
-            'model': vehicle_data['naming']['model'],
-            'version': vehicle_data['naming']['version'],
-            'battery_capacity': vehicle_data['battery']['usable_kwh'],
-            'range': vehicle_data['range']['chargetrip_range']['best'],
-            'consumption': vehicle_data['battery']['usable_kwh'] / vehicle_data['range']['chargetrip_range']['best']
-        }
+    car_specs = {
+        'battery_capacity': selected_vehicle['battery']['usable_kwh'],
+        'optimal_range': selected_vehicle['range']['chargetrip_range']['best'],
+        'efficiency': selected_vehicle['battery']['usable_kwh'] / selected_vehicle['range']['chargetrip_range']['best']
+    }
 
-        # Récupérer l'autonomie et la consommation par km
-        vehicle_autonomy = vehicle_data['range']['chargetrip_range']['best']
-        vehicle_consumption_per_km = vehicle_data['battery']['usable_kwh'] / vehicle_autonomy
+    start_coords = get_location_coordinates(origin_city)
+    end_coords = get_location_coordinates(destination_city)
+    if not start_coords or not end_coords:
+        return jsonify({'error': 'Invalid coordinates'}), 400
 
-        # Obtenir les coordonnées de départ et d'arrivée
-        coords_depart = get_coordinates(ville_depart)
-        coords_arrivee = get_coordinates(ville_arrivee)
+    params = {
+        'point': [f"{start_coords['lat']},{start_coords['lng']}", f"{end_coords['lat']},{end_coords['lng']}"],
+        'vehicle': 'car',
+        'key': API_KEY_ROUTE,
+        'instructions': 'false',
+        'points_encoded': 'false'
+    }
+    initial_route_response = requests.get(ROUTING_SERVICE_URL, params=params)
+    initial_route_response.raise_for_status()
 
-        if not coords_depart or not coords_arrivee:
-            return jsonify({'error': 'Invalid coordinates'}), 400
+    route_details = initial_route_response.json()
+    route_path = extract_route_coordinates(route_details)
 
-        # Requête initiale pour l'itinéraire
-        params = {
-            'point': [f"{coords_depart['lat']},{coords_depart['lng']}", f"{coords_arrivee['lat']},{coords_arrivee['lng']}"],
-            'vehicle': 'car',
-            'key': GRAPHOPPER_API_KEY,
-            'instructions': 'false',
-            'points_encoded': 'false'
-        }
-        response = requests.get(GRAPHOPPER_URL, params=params)
-        response.raise_for_status()
+    # Localiser les bornes de recharge
+    charging_stations = locate_stations(route_path, 200, car_specs['efficiency'], selected_vehicle)
 
-        route_data = response.json()
-        coords_itineraire = parse_coordinates(route_data)
+    # Ajouter les bornes dans l'itinéraire
+    via_points = [f"{station['location']['coordinates'][1]},{station['location']['coordinates'][0]}" for station in charging_stations]
+    if via_points:
+        params['point'] = [f"{start_coords['lat']},{start_coords['lng']}"] + via_points + [f"{end_coords['lat']},{end_coords['lng']}"]
 
-        # Calcul des stations de recharge en fonction de l'autonomie "worst"
-        stations = find_charging_stations(coords_itineraire, 200, vehicle_consumption_per_km, vehicle_data)
+        # Recalculer l'itinéraire avec les points intermédiaires
+        final_route_response = requests.get(ROUTING_SERVICE_URL, params=params)
+        final_route_response.raise_for_status()
+        route_details = final_route_response.json()
+        route_path = extract_route_coordinates(route_details)
 
-        # Construire la réponse avec les points de passage
-        via_points = [f"{station['location']['coordinates'][1]},{station['location']['coordinates'][0]}" for station in stations]
-        if via_points:
-            params['point'] = [f"{coords_depart['lat']},{coords_depart['lng']}"] + via_points + [f"{coords_arrivee['lat']},{coords_arrivee['lng']}"]
+    duration_ms = route_details['paths'][0]['time']
+    hours = duration_ms // 3600000
+    minutes = (duration_ms % 3600000) // 60000
 
-        response = requests.get(GRAPHOPPER_URL, params=params)
-        response.raise_for_status()
+    return jsonify({
+        'total_distance': f"{route_details['paths'][0]['distance'] / 1000:.2f} km",
+        'travel_time': f"{hours}h {minutes}m",
+        'route_path': route_path,
+        'charging_stations': charging_stations,
+        'car_specs': car_specs
+    })
 
-        route_data_with_stops = response.json()
-        coords_itineraire = parse_coordinates(route_data_with_stops)
-
-        # Calcul de la durée en heures et minutes
-        duration_ms = route_data_with_stops['paths'][0]['time']
-        hours = duration_ms // 3600000
-        minutes = (duration_ms % 3600000) // 60000
-
-        return jsonify({
-            'distance': f"{route_data_with_stops['paths'][0]['distance'] / 1000:.2f} km",
-            'duration': f"{hours}h {minutes}m",
-            'route': coords_itineraire,
-            'stations': stations,
-            'vehicle_details': vehicle_details  # Inclure les détails du véhicule
-        })
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-def get_coordinates(city):
-    response = requests.get(f"https://graphhopper.com/api/1/geocode?q={city}&key={GRAPHOPPER_API_KEY}")
-    if response.status_code == 200:
-        data = response.json()
-        if data['hits']:
-            return data['hits'][0]['point']
+def get_location_coordinates(city_name):
+    response = requests.get(f"https://graphhopper.com/api/1/geocode?q={city_name}&key={API_KEY_ROUTE}")
+    response.raise_for_status()
+    data = response.json()
+    if data['hits']:
+        return data['hits'][0]['point']
     return None
 
-def parse_coordinates(route_data):
-    return [[coord[1], coord[0]] for coord in route_data['paths'][0]['points']['coordinates']]
+def extract_route_coordinates(route_response):
+    if 'paths' not in route_response or not route_response['paths']:
+        raise ValueError("No paths found in the route response")
+    coordinates = route_response['paths'][0]['points']['coordinates']
+    if not coordinates:
+        raise ValueError("No coordinates found in the path")
+    return [[point[1], point[0]] for point in coordinates]
 
-def find_charging_stations(itinerary, distance_between_stations, consumption_per_km, vehicle_data):
-    # Utilisation de l'autonomie la plus faible pour définir la distance
-    worst_range = vehicle_data['range']['chargetrip_range']['worst']
-    stations = []
-    current_distance = 0
+def locate_stations(route_path, max_distance, efficiency, car_info):
     headers = {
-        'x-client-id': CHARGETRIP_CLIENT_KEY,
-        'x-app-id': CHARGETRIP_APP_KEY,
-        'Authorization': f'Bearer {CHARGETRIP_CLIENT_KEY}',
+        'x-client-id': EV_CHARGING_CLIENT_ID,
+        'x-app-id': EV_CHARGING_APP_ID,
+        'Authorization': f'Bearer {EV_CHARGING_CLIENT_ID}',
         'Content-Type': 'application/json'
     }
+    stations = []
+    current_distance = 0
+    max_range = car_info['range']['chargetrip_range']['worst']
 
-    # Calculer la distance entre chaque station en fonction de l'autonomie "worst"
-    max_distance = worst_range  # Utiliser l'autonomie "worst" pour déterminer la distance entre chaque station
-
-    for i in range(len(itinerary) - 1):
-        segment_distance = haversine_distance(itinerary[i], itinerary[i + 1])
+    for i in range(len(route_path) - 1):
+        segment_distance = haversine_distance(route_path[i], route_path[i + 1])
         current_distance += segment_distance
 
-        # Ajouter une station de recharge lorsque la distance dépasse l'autonomie "worst"
-        if current_distance >= max_distance:
-            station_location = itinerary[i]
+        if current_distance >= max_range:
+            station_location = route_path[i]
             query = f"""
             query {{
                 stationAround(
@@ -200,21 +175,24 @@ def find_charging_stations(itinerary, distance_between_stations, consumption_per
                 }}
             }}
             """
-            response = requests.post(CHARGETRIP_URL, json={'query': query}, headers=headers)
+            response = requests.post(EV_CHARGING_ENDPOINT, json={'query': query}, headers=headers)
             if response.status_code == 200:
                 station_data = response.json().get('data', {}).get('stationAround', [])
                 if station_data:
                     stations.append(station_data[0])
-                    current_distance = 0  # Réinitialiser la distance après un arrêt de recharge
+                    current_distance = 0
 
     return stations
 
 def haversine_distance(coord1, coord2):
-    R = 6371  # Rayon de la Terre en km
+    if not coord1 or not coord2 or len(coord1) != 2 or len(coord2) != 2:
+        raise ValueError("Invalid coordinates provided for haversine calculation")
+
+    R = 6371
     dlat = radians(coord2[0] - coord1[0])
     dlon = radians(coord2[1] - coord1[1])
     a = sin(dlat / 2)**2 + cos(radians(coord1[0])) * cos(radians(coord2[0])) * sin(dlon / 2)**2
     return R * 2 * atan2(sqrt(a), sqrt(1 - a))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
